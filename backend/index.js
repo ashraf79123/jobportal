b7631c9343de4ff0034bea030899c7b437edcac5
mohd ashraf <ashrafmohd8756328465@gmail.com>
2025-05-23 20:13:20 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-1-jchc.onrender.com',
     credentials:true
 }
 
